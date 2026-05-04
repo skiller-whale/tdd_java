6837915e9303ff1a9d3701ede02145dbbd34747d
neoeno <1007202+neoeno@users.noreply.github.com>
2026-05-04 12:13:48 +0000

    Sync exercise files

--- exercises/inside_out_tdd/src/main/java/com/example/WordleApp.java
+++ exercises/inside_out_tdd/src/main/java/com/example/WordleApp.java
@@ -3,6 +3,12 @@ package com.example;
 import java.util.List;
 
 public class WordleApp {
+    private final String wordFilePath;
+
+    public WordleApp(String wordFilePath) {
+        this.wordFilePath = wordFilePath;
+    }
+
     public List<String> run(String[] args) {
         return List.of();
     }
